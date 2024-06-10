3f1bc804ce5a7bd3aa40247e779881d65e7e8365
Adna <adna.mehanovic1@gmail.com>
2024-06-10 20:24:55 +0200

    Add register component

---
+++ nwtfrontend/src/app/register/register.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+  
+  constructor(private router: Router) {}
+  
+  navigate(route: string) {
+    this.router.navigate([route]);
+  }
+}
