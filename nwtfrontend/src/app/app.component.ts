cccbb3a80cd71a67b5c8a02dbfebddfec2c67310
Adna <adna.mehanovic1@gmail.com>
2024-06-10 20:27:35 +0200

    Updated navbar and added image

--- nwtfrontend/src/app/app.component.ts
+++ nwtfrontend/src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, HomeComponent, LoginComponent, RegisterComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'nwtfrontend';
+
+  constructor(private router: Router) {}
+  
+  navigate(route: string) {
+    this.router.navigate([route]);
+  }
 }
--- nwtfrontend/src/app/navbar/navbar.component.ts
+++ nwtfrontend/src/app/navbar/navbar.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { LoginComponent } from '../login/login.component';
+import { RegisterComponent } from '../register/register.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, LoginComponent, RegisterComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
+
 export class NavbarComponent {
 
+  constructor(private router: Router) {}
+  
+  navigate(route: string) {
+    this.router.navigate([route]);
+  }
+  
+  
 }
