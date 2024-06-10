6f40dd435051487ac72089a42ae46409c86175f9
Adna <adna.mehanovic1@gmail.com>
2024-06-10 20:24:02 +0200

    Add home component

---
+++ nwtfrontend/src/app/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '../navbar/navbar.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+
+export class HomeComponent {
+
+}
