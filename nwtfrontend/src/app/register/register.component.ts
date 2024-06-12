40099b8e1aadfab427861b9cf48b489592874f44
Adna <adna.mehanovic1@gmail.com>
2024-06-12 16:58:45 +0200

    Registration updated

--- nwtfrontend/src/app/register/register.component.ts
+++ nwtfrontend/src/app/register/register.component.ts
@@ -1,18 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthenticationResponse } from './register';
+import { HttpErrorResponse } from '@angular/common/http';
+import { RegisterService } from './register.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
   
-  constructor(private router: Router) {}
-  
+  errors: { [key: string]: string } = {};
+
+  constructor(private router: Router, private registerService: RegisterService) {}
+
   navigate(route: string) {
     this.router.navigate([route]);
+  }
+
+  public registerUser(addForm: NgForm): void {
+    this.registerService.registerUser(addForm.value).subscribe(
+      (response: AuthenticationResponse) => {
+        console.log(response);
+        this.errors = {};
+        addForm.reset();
+        this.navigate('/login');
+      },
+      (error: HttpErrorResponse) => {
+        if (error.status === 400 && error.error) {
+          this.errors = error.error;
+        } else {
+          alert('Došlo je do greške. Pokušajte ponovo.');
+        }
+      }
+    );
   }
 }
--- nwtfrontend/src/app/register/register.service.ts
+++ nwtfrontend/src/app/register/register.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthenticationResponse, RegisterRequest } from './register';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
+  
+ // private apiServerUrl = environment.apiBaseUrl;
 
-  constructor() { }
+ // private baseUrl = 'http://localhost:8000/auth';
+
+  constructor(private http: HttpClient) { }
+
+  registerUser(registerData: RegisterRequest): Observable<AuthenticationResponse> {
+    return this.http.post<AuthenticationResponse>('http://localhost:8000/auth/auth/register-patient', registerData);
+  }
 }
